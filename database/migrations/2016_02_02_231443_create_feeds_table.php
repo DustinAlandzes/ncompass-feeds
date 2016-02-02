7442469648309b44be7468bea30659bdbc96d396
Dustin Alandzes <dustin@alandz.es>
2016-02-02 17:22:24 -0600

    Feed model, etc

---
+++ database/migrations/2016_02_02_231443_create_feeds_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feeds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('feeds');
+    }
+}
