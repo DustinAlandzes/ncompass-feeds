58daab06e102753b8c739910e3b51dde1f9d17c4
f00 <f00@gibson>
2016-07-19 17:00:22 -0500

    added firebug back in

--- resources/views/feeds/inoreader.blade.php
+++ resources/views/feeds/inoreader.blade.php
@@ -75,9 +75,7 @@
   </div>
     <div id="description">
     </div>
-    <!--
-    <script type="text/javascript" src="https://getfirebug.com/firebug-lite-debug.js"></script>
-    -->
+    <script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>
     <script type="text/javascript" charset="utf-8">
 
         function setBackground(url)
